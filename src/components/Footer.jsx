9fd3c9a1f7439200d8ce151915937d3250493f6d
Nishant kumar <nishant51121@gmail.com>
2024-07-09 21:02:47 +0530

    add credit and debit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,47 +1,61 @@
 import React, { useState } from 'react';
 import '../assets/css/footer.css';
-import TransactionModal from './TransactionModal'; // Adjust the path as needed
-import {updateTransactionDetails} from '../assets/data/transaction.js';
-function Footer() {
-    const [isModalVisible, setIsModalVisible] = useState(false);
+import TransactionModal from './TransactionModal';
+import TransactionCredit from './TransactionCredit.jsx';
+import { updateTransactionDetails } from '../assets/data/transaction.js';
 
-    const handleAddTransaction = () => {
-        setIsModalVisible(true);
+function Footer({ onAddTransaction }) {
+    const [isCreditModalVisible, setIsCreditModalVisible] = useState(false);
+    const [isDebitModalVisible, setIsDebitModalVisible] = useState(false);
+
+    const handleAddCreditTransaction = () => {
+        setIsCreditModalVisible(true);
     };
 
-    const handleAddNew = () => {
-        setIsModalVisible(true);
+    const handleAddDebitTransaction = () => {
+        setIsDebitModalVisible(true);
     };
 
-    const handleUpdateTransaction = () => {
-        setIsModalVisible(true);
+    const handleCloseCreditModal = () => {
+        setIsCreditModalVisible(false);
     };
 
-    const handleCloseModal = () => {
-        setIsModalVisible(false);
+    const handleCloseDebitModal = () => {
+        setIsDebitModalVisible(false);
     };
 
-    const handleSubmitTransaction = (transactionDetails) => {
-        console.log("New transaction data: ", transactionDetails);
+    const handleSubmitCreditTransaction = (transactionDetails) => {
+        console.log("New credit transaction data: ", transactionDetails);
         updateTransactionDetails();
-        // Add your logic for submitting the transaction here
-        setIsModalVisible(false);
+        setIsCreditModalVisible(false);
+    };
+
+    const handleSubmitDebitTransaction = (transactionDetails) => {
+        console.log("New debit transaction data: ", transactionDetails);
+        updateTransactionDetails();
+        setIsDebitModalVisible(false);
     };
 
     return (
         <>
-        <TransactionModal 
-                isVisible={isModalVisible} 
-                onClose={handleCloseModal} 
-                onSubmit={handleSubmitTransaction} 
-            />
-            <div className="footer">
-                <div className="Add-trans trans" onClick={handleAddTransaction}>
-                    <p title="Add Transaction">AT+</p>
-                </div>
-                <div title="Add New" className="plus-new" onClick={handleAddNew}>+</div>
-                <div className="update-trans trans" onClick={handleUpdateTransaction}>
-                    <p title="Update Transaction">UT+</p>
+            <div className='bottom-menu'>
+                <TransactionModal
+                    isVisible={isDebitModalVisible}
+                    onClose={handleCloseDebitModal} 
+                    onSubmit={handleSubmitDebitTransaction} 
+                />
+                <TransactionCredit
+                    isVisible={isCreditModalVisible}
+                    onClose={handleCloseCreditModal} 
+                    onSubmit={handleSubmitCreditTransaction} 
+                />
+                <div className="footer">
+                    <div className="add-trans" onClick={handleAddCreditTransaction}>
+                        <p title="Add Credit Transaction" className='trans'>Credit</p>
+                    </div>
+                    <div title="Add Debit Transaction" className="add-trans add-plus" onClick={handleAddDebitTransaction}>
+                        <p title="Add Debit Transaction" className='trans'>Debit</p>
+                    </div>
                 </div>
             </div>
         </>
