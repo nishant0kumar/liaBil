a6263ef8dc274fb9838ee9574a1a46e0e3221bfd
Nishant kumar <nishant51121@gmail.com>
2024-07-12 17:04:42 +0530

    theme addition

---
+++ src/components/DarkMode.jsx
@@ -0,0 +1,15 @@
+import '../assets/css/dark-mode.css'
+
+function DarkMode() {
+    return(
+        <div>
+            <div>
+                <button className='dark-but'>
+                    <i className='fa-solid fa-sun'></i>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default DarkMode;
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,30 +1,36 @@
 import '../assets/css/navigation.css';
 import logo from '../../public/liabil.svg'
 
+import DarkMode from './DarkMode';
+
 function NavigationBar() {
     return (
         <>
             <nav className='nav-bar'>
-            <div className='logo'>
-                <img src={logo} alt="logo" className='logo-liabil'/>
-            </div>
-            <li>
-                <i className='fa-solid fa-house'></i>
-                <p>Home</p>
-            </li>
-            <li>
-                <i className='fa-solid fa-money-bill-wave'></i>
-                <p>Account</p>
-            </li>
-            <li>
-                <i className='fa-solid fa-history'></i>
-                <p>History</p>
-            </li>
-            <li>
-                <i className='fa-solid fa-chart-area'></i>
-                <p>Graph</p>
-            </li>
-        </nav>
+                <div className='logo'>
+                    <img src={logo} alt="logo" className='logo-liabil'/>
+                </div>
+                <ul className='nav-bar'>
+                    <li>
+                        <i className='fa-solid fa-house'></i>
+                        <p>Home</p>
+                    </li>
+                    <li>
+                        <i className='fa-solid fa-money-bill-wave'></i>
+                        <p>Account</p>
+                    </li>
+                    <li>
+                        <i className='fa-solid fa-history'></i>
+                        <p>History</p>
+                    </li>
+                    <li>
+                        <i className='fa-solid fa-chart-area'></i>
+                        <p>Graph</p>
+                    </li>
+                </ul>
+
+                <DarkMode/>
+            </nav>
         </>
     )
 }
